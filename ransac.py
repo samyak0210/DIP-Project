dcde46ca76df59ac4643354084d48b537d0e9769
Mudit Surana <suranamudit@hotmail.com>
2018-10-31 23:33:47 +0530

    updated ransac.py

---
+++ ransac.py
@@ -0,0 +1,68 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from random import choice
+from scipy import ndimage
+import scipy
+from PIL import Image
+import random
+
+
+def calculateHomography(correspondences):
+    aList = []
+    for corr in correspondences:
+        p1 = np.matrix([corr.item(0), corr.item(1), 1])
+        p2 = np.matrix([corr.item(2), corr.item(3), 1])
+
+        a2 = [0, 0, 0, -p2.item(2) * p1.item(0), -p2.item(2) * p1.item(1), -p2.item(2) * p1.item(2),
+              p2.item(1) * p1.item(0), p2.item(1) * p1.item(1), p2.item(1) * p1.item(2)]
+        a1 = [-p2.item(2) * p1.item(0), -p2.item(2) * p1.item(1), -p2.item(2) * p1.item(2), 0, 0, 0,
+              p2.item(0) * p1.item(0), p2.item(0) * p1.item(1), p2.item(0) * p1.item(2)]
+        aList.append(a1)
+        aList.append(a2)
+
+    matrixA = np.matrix(aList)
+
+    u, s, v = np.linalg.svd(matrixA)
+
+    h = np.reshape(v[8], (3, 3))
+
+    h = (1/h.item(8)) * h
+    return h
+
+def geometricDistance(correspondence, h):
+    p1 = np.transpose(np.matrix([correspondence[0].item(0), correspondence[0].item(1), 1]))
+    estimatep2 = np.dot(h, p1)
+    estimatep2 = (1/estimatep2.item(2))*estimatep2
+
+    p2 = np.transpose(np.matrix([correspondence[0].item(2), correspondence[0].item(3), 1]))
+    error = p2 - estimatep2
+    return np.linalg.norm(error)
+
+def ransac(corr, thresh, dist=5):
+    maxInliers = []
+    finalH = None
+    for i in range(1000):
+        corr1 = corr[random.randrange(0, len(corr))]
+        corr2 = corr[random.randrange(0, len(corr))]
+        randomFour = np.vstack((corr1, corr2))
+        corr3 = corr[random.randrange(0, len(corr))]
+        randomFour = np.vstack((randomFour, corr3))
+        corr4 = corr[random.randrange(0, len(corr))]
+        randomFour = np.vstack((randomFour, corr4))
+        h = calculateHomography(randomFour)
+
+        inliers = []
+
+        for cr in corr:
+            d = geometricDistance(cr, h)
+            if d < dist:
+                inliers.append(cr)
+
+        if len(inliers) > len(maxInliers):
+            maxInliers = inliers
+            finalH = h
+
+        if len(maxInliers) > (len(corr)*thresh):
+            break
+    return finalH, maxInliers
