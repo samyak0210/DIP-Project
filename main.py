28d561569aa618e84607545670d3e8090a91cab3
samyak0210 <smkjain8@gmail.com>
2018-11-17 20:52:05 +0530

    updated main

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ import scipy
 from PIL import Image
 import random
 from ransac import *
+from seam import *
+from tqdm import *
 
 def main():
-    im1 = cv2.imread('dataset/3-1.jpg', 0)
-    im2 = cv2.imread('dataset/3-2.jpg', 0)
+    im1 = cv2.imread('1.jpg', 0)
+    im2 = cv2.imread('2.jpg', 0)
     im1_copy = im1.copy()
     im1 = cv2.copyMakeBorder(im1,200,200,500,500, cv2.BORDER_CONSTANT)
     
@@ -32,21 +34,36 @@ def main():
     
     
     corrs = np.matrix(correspondenceList)
-    out_ransac, inliers = ransac(corrs, 5.0)
-    out = cv2.warpPerspective(im2, scipy.linalg.inv(out_ransac), (im1.shape[1],  im1.shape[0]))
+    out_arr, inliers = ransac(corrs, 5.0)
+    print(len(out_arr))
+    mi = np.inf
+    final_image = None
+    for i in trange(len(out_arr)):
+        out_ransac = out_arr[i]
+        out = cv2.warpPerspective(im2, scipy.linalg.inv(out_ransac), (im1.shape[1],  im1.shape[0]))
+        output = np.zeros_like(im1)
+        (x, y) = im1.shape
+        seam = []
+        for i in range(x):
+            for j in range(y):
+                if im1[i][j]==0 and out[i][j]==0:
+                    output[i][j]=0
+                elif im1[i][j]==0:
+                    output[i][j] = out[i][j]
+                elif out[i][j]==0:
+                    output[i][j] = (im1[i][j])
+                else:
+                    output[i,j] = energy(im1, out, 128, 2, i, j)
+                    seam.append([i,j])
+                    # output[i][j] = (int(int(im1[i][j]) + int(out[i][j]))/2)
+        error = 0
+        for [x, y] in seam:
+            error += np.sum((output[x-8:x+9, y-8:y+9] - im1[x-8:x+9, y-8:y+9])**2 + (output[x-8:x+9, y-8:y+9] - out[x-8:x+9, y-8:y+9])**2)
+        if error/len(seam) < mi:
+            print("hi")
+            mi = error/len(seam)
+            final_image = np.copy(output)
 
-    output = np.zeros_like(im1)
-    (x, y) = im1.shape
-    for i in range(x):
-        for j in range(y):
-            if im1[i][j]==0 and out[i][j]==0:
-                output[i][j]=0
-            elif im1[i][j]==0:
-                output[i][j] = out[i][j]
-            elif out[i][j]==0:
-                output[i][j] = (im1[i][j])
-            else:
-                output[i][j]= (int(int(im1[i][j]) + int(out[i][j]))/2)
 
     plt.subplot(2,2,1)
     plt.axis('off')
@@ -58,7 +75,7 @@ def main():
 
     plt.subplot(2,2,3)
     plt.axis('off')
-    plt.imshow(output, cmap='gray')
+    plt.imshow(final_image, cmap='gray')
 
     plt.subplot(2,2,4)
     plt.axis('off')
@@ -66,7 +83,7 @@ def main():
 
     plt.show()
 
-    cv2.imwrite('results/result3.jpg', output)
+    cv2.imwrite('result1.jpg', final_image)
 
 if __name__ == "__main__":
     main()
